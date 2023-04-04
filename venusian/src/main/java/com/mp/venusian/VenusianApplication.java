98de04da7f071973a782c4f55715203b1c7df305
Mario Morais Neto <mariom_mmn@hotmail.com>
2023-04-03 23:00:11 -0300

    first

---
+++ venusian/src/main/java/com/mp/venusian/VenusianApplication.java
@@ -0,0 +1,13 @@
+package com.mp.venusian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenusianApplication.class, args);
+	}
+
+}
---
+++ venusian/src/test/java/com/mp/venusian/VenusianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mp.venusian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
